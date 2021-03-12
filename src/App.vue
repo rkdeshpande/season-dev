07de184c66c3ea77e2dcff90277003f1143a8873
Rohan K. Deshpande <Deshpande.Rohan@bcg.com>
2021-03-12 11:49:05 -0500

    Dev init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <font-awesome-icon class="season-icon" :icon="['fas', 'cloud-showers-heavy']" />
+    <font-awesome-icon class="season-icon" :icon="['fas', 'sun']" />
+    <font-awesome-icon class="season-icon" :icon="['fas', 'wind']" />
+    <font-awesome-icon class="season-icon" :icon="['fas', 'snowflake']" />
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/TitleCard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.season-icon {
+  font-size: 5em;
+  margin-left: 40px;
+}
+.season-icon:first-child {
+  margin-left: 0;
+}
+</style>
