ab6c6f1f7c45fd0c88cfe2adbde4955c2d458305
Rohan Deshpande <rkdeshpande12@gmail.com>
2025-05-20 17:27:09 -0400

    linting

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  files: ['src/**/*.{js,jsx,ts,tsx}'],
+  plugins: ['react', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'react-app',
+    'react-app/jest'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}; 
